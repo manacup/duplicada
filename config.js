4641b5a0c7be6071be8c6e02d9022a70dcc5aa06
manacup <91571517+manacup@users.noreply.github.com>
2025-05-16 12:04:00 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+        // Configuració de Firebase
+        const firebaseConfig = {
+    apiKey: "AIzaSyAwsV6cBafAt6OQRNUEFXCoRT-D5Fzvqbk",
+    authDomain: "duplicadascrabble.firebaseapp.com",
+    databaseURL: "https://duplicadascrabble-default-rtdb.europe-west1.firebasedatabase.app", // Corrected URL            
+    projectId: "duplicadascrabble",
+    storageBucket: "duplicadascrabble.firebasestorage.app",
+    messagingSenderId: "115691804214",
+    appId: "1:115691804214:web:dd923cf73c46146c0b2a97"
+        };
