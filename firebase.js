ad26b98023b334437adfdf3054388e5e0c69601b
manacup <91571517+manacup@users.noreply.github.com>
2025-07-31 11:07:23 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Inicialització de Firebase i exportació de referències útils
 
 // Assegura't d'afegir el teu objecte de configuració de Firebase aquí:
-const firebaseConfig = {
+/* const firebaseConfig = {
     apiKey: "AIzaSyAwsV6cBafAt6OQRNUEFXCoRT-D5Fzvqbk",
     authDomain: "duplicadascrabble.firebaseapp.com",
     databaseURL: "https://duplicadascrabble-default-rtdb.europe-west1.firebasedatabase.app", // Corrected URL            
@@ -9,7 +9,23 @@ const firebaseConfig = {
     storageBucket: "duplicadascrabble.firebasestorage.app",
     messagingSenderId: "115691804214",
     appId: "1:115691804214:web:dd923cf73c46146c0b2a97"
-};
+}; */
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyBbWqeWhQgK5C0Ioj7-uSNx9q0i8VZmIJM",
+
+    authDomain: "duplicadaporreres.firebaseapp.com",
+
+    projectId: "duplicadaporreres",
+
+    storageBucket: "duplicadaporreres.firebasestorage.app",
+
+    messagingSenderId: "835862262761",
+
+    appId: "1:835862262761:web:4465dadd41f0f31edd1178"
+
+  };
+
 
 // Inicialitza Firebase només si no està inicialitzat
 if (!window.firebase?.apps?.length) {
